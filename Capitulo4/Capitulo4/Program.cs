3b8aea1b7f0daa70811359bd723591dbabf16302
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-08-17 19:32:18 -0300

    Nelio Alves Course

---
+++ Capitulo4/Capitulo4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Capitulo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario1;
+            Funcionario funcionario2;
+
+            funcionario1 = new Funcionario();
+            funcionario2 = new Funcionario();
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            funcionario1.Nome = Console.ReadLine();
+            Console.Write("Sálario: ");
+            funcionario1.Salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            funcionario2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionario2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("A média dos salários é " + ((funcionario1.Salario + funcionario2.Salario)/2).ToString("F2",CultureInfo.InvariantCulture));
+            
+            
+        }
+    }
+}
---
+++ Primeiro/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            x.A = 4.00;
+        }
+    }
+}
