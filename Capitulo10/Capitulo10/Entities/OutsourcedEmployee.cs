5a8f69043646250f8965ed44ba8c024ab93d2343
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-09-01 18:27:43 -0300

    Inheritance and polyformism

---
+++ Capitulo10/Capitulo10/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capitulo10.Entities
+{
+    class OutsourcedEmployee:Employee
+    {
+        public double AdicionalCharge { get; set; }
+
+        public OutsourcedEmployee() { }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour,double adicionalCharge)
+            :base(name,hours,valuePerHour)
+        {
+            AdicionalCharge = adicionalCharge;
+        }
+
+        public override double payment()
+        {
+            return base.payment() + (AdicionalCharge * 1.1);
+        }
+    }
+}
---
+++ Capitulo10/Capitulo10/Program.cs
@@ -0,0 +1,54 @@
+﻿using Capitulo10.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Capitulo10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercice1();
+        }
+
+        static void Exercice1()
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{1} data:");
+                Console.Write("Outsourced (y/n)? ");
+                char yN = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine());
+                if (yN == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine());
+                    Employee employee = new OutsourcedEmployee(name,hours,valuePerHour,additionalCharge);
+                    employees.Add(employee);
+                    
+                }else if(yN == 'n')
+                {
+                    Employee employee = new Employee(name, hours, valuePerHour);
+                    employees.Add(employee);
+                }
+
+                
+            }
+
+            foreach (Employee employee in employees)
+            {
+                Console.Write($"{employee.Name} - $ {employee.payment()} ");
+            }
+        }
+    }
+}
