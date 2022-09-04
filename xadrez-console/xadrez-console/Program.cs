28460ea44e9ee5f0eac62b00475cf02867c7770f
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-09-04 14:13:01 -0300

    Classe Posicao Criada

--- xadrez-console/xadrez-console/Program.cs
+++ xadrez-console/xadrez-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace xadrez_console
 {
@@ -6,7 +7,14 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            teste();
+        }
+
+        static void teste()
+        {
+            Posicao posicao = new Posicao(3, 4);
+            Console.WriteLine(posicao);
+            Console.ReadLine();
         }
     }
 }
