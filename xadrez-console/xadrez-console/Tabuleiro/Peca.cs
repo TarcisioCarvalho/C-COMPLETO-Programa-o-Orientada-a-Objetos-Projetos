1da5cb8a86fa3d80b7603fa65ab94274511fbd07
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-09-04 15:18:26 -0300

    Comecando a implmentar o tabuleiro

---
+++ xadrez-console/xadrez-console/Tabuleiro/Peca.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using xadrez_console.tabuleiro.Enums;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Cor Cor { get; set; }
+        public Posicao Posicao { get; protected set; }
+        public Tabuleiro tabuleiro { get; set; }
+    }
+}
