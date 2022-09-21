ac929296ae128d621b715ed9f97589c70072edcc
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-09-21 19:30:37 -0300

    MySQL adaptation and first migration

--- SallesWebMvc/SallesWebMvc/Startup.cs
+++ SallesWebMvc/SallesWebMvc/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SallesWebMvc.Data;
 
 namespace SallesWebMvc
 {
@@ -33,6 +35,10 @@ namespace SallesWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SallesWebMvcContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("SallesWebMvcContext"), builder =>
+                    builder.MigrationsAssembly("SallesWebMvc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
