26c52916e2111266c1be7cec020b34b69e41c26e
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-09-28 19:28:57 -0300

    Simple Create Form

--- SallesWebMvc/SallesWebMvc/Controllers/SellersController1.cs
+++ SallesWebMvc/SallesWebMvc/Controllers/SellersController1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SallesWebMvc.Models;
 using SallesWebMvc.Services;
 using System;
 using System.Collections.Generic;
@@ -21,6 +22,19 @@ namespace SallesWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateSeller(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
