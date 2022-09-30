b65ee31158d735096340cf8814d4b1be6251b89d
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-09-30 11:56:16 -0300

    Delete seller

--- SallesWebMvc/SallesWebMvc/Services/SellerService.cs
+++ SallesWebMvc/SallesWebMvc/Services/SellerService.cs
@@ -27,5 +27,16 @@ namespace SallesWebMvc.Services
             _context.Add(seller);
             _context.SaveChanges();
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(seller => seller.Id == id);
+        }
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Remove(obj);
+            _context.SaveChanges();
+        }
     }
 }
