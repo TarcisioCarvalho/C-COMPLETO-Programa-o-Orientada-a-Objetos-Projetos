19f7c1b071d13b1841dc579c5f364e3fb0fc31d4
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-10-02 19:58:37 -0300

    Validation

--- SallesWebMvc/SallesWebMvc/Models/Seller.cs
+++ SallesWebMvc/SallesWebMvc/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 
@@ -8,9 +9,22 @@ namespace SallesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="{0} Required")]
+        [StringLength(60,MinimumLength = 3,ErrorMessage ="{0} size should be between {2} and {1}")]
         public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} Required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         public string Email { get; set; }
+        [Display(Name ="Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "{0} Required")]
         public DateTime BirthDate { get; set; }
+        [Display(Name ="Base Salary")]
+        [DisplayFormat(DataFormatString ="{0:F2}")]
+        [Required(ErrorMessage = "{0} Required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
